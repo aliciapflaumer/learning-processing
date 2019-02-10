a1fd99d98956408b13b2d2a06717228ebf8186f0
aliciapflaumer <alpfla7@gmail.com>
2019-02-10 11:28:08 -0500

    Update to p5jsMode

--- modes/p5jsMode/examples/Image/ex01_Background_Image/ex01_Background_Image.js
+++ modes/p5jsMode/examples/Image/ex01_Background_Image/ex01_Background_Image.js
@@ -1,20 +1,20 @@
 /*
  * @name Background Image
  * @description This example presents the fastest way to load a
- * background image into Processing. To load an image as the background,
+ * background image. To load an image as the background,
  * it must be the same width and height as the program.
  * <p><em><span class="small"> To run this example locally, you will need an
  * image file, and a running <a href="https://github.com/processing/p5.js/wiki/Local-server">
  * local server</a>.</span></em></p>
  */
-var bg;
-var y = 0;
+let bg;
+let y = 0;
 
 function setup() {
   // The background image must be the same size as the parameters
   // into the createCanvas() method. In this program, the size of
   // the image is 720x400 pixels.
-  bg = loadImage("data/moonwalk.jpg");
+  bg = loadImage('data/moonwalk.jpg');
   createCanvas(720, 400);
 }
 
