36e111ecb382372cf978bd6c87e36c3690085663
aliciapflaumer <alpfla7@gmail.com>
2019-03-03 14:10:26 -0500

    Draw button

--- exercises/chapter-9/button/build/source/button.java
+++ exercises/chapter-9/button/build/source/button.java
@@ -14,63 +14,21 @@ import java.io.IOException;
 
 public class button extends PApplet {
 
-class Button {
-  float x;
-  float y;
-  float w;
-  float h;
-  boolean on;
 
-
-  Button(float tempX, float tempY, float tempW, float tempH) {
-    x = tempX;
-    y = tempY;
-    w = tempW;
-    h = tempH;
-
-    on = false;
-  }
-
-  public void register() {
-    if (on) {
-      background(255);
-      stroke(0);
-    } else {
-      background(0);
-      stroke(255);
-    }
-    fill(175);
-    ellipse(x, y, w, h);
-  }
-
-
-}
 public void settings() {
   size(300,300);
 }
 
-Button [] buttons = new Button[1];
 
 public void setup() {
-  buttons[0] = new Button(100, 100, 20, 20);
+  background(100);
 }
 
 public void draw() {
-  background(100);
-
-  for (int i = 0; i < buttons.length; i++) {
-    // buttons[i].mousePressed(mouseX, mouseY);
-    buttons[i].register();
-  }
+  fill(255);
+  noStroke();
+  ellipse(100, 100, 20, 20);
 }
-
-// void mousePressed() {
-//   // A new button object
-//   Button b = new Button(mouseX, mouseY, 10);
-//
-//   // Append to array
-//   buttons = (Ball[]) append(buttons, b);
-// }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "button" };
     if (passedArgs != null) {
--- exercises/chapter-9/button/objectArray.pde
+++ exercises/chapter-9/button/objectArray.pde
@@ -2,25 +2,13 @@ void settings() {
   size(300,300);
 }
 
-Button [] buttons = new Button[1];
 
 void setup() {
-  buttons[0] = new Button(100, 100, 20, 20);
+  background(100);
 }
 
 void draw() {
-  background(100);
-
-  for (int i = 0; i < buttons.length; i++) {
-    // buttons[i].mousePressed(mouseX, mouseY);
-    buttons[i].register();
-  }
+  fill(255);
+  noStroke();
+  ellipse(100, 100, 20, 20);
 }
-
-// void mousePressed() {
-//   // A new button object
-//   Button b = new Button(mouseX, mouseY, 10);
-//
-//   // Append to array
-//   buttons = (Ball[]) append(buttons, b);
-// }
